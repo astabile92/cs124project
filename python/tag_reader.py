c0ac9c1e2a415b466d40858c3ebadcb9a7e4db1c
astabile92 <astabile@stanford.edu>
2014-02-24 22:47:10 -0800

    Part of Speech Tagging
    Created class TagReader, which builds the corpus using a file annotated
    with part-of-speech tagging, as produced by TreeTagger (see
    two_sentences_tagged.txt)
    
    Edited the Translator class to use this tagged corpus when generating
    translations.

---
+++ python/tag_reader.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+import sys
+
+class TagReader:
+
+	def __init__(self):
+		"""
+		***How the Corpus works***
+		self.corpus is a list of sentences, where each sentence is a list of "words":
+		A "word" is a triple in the format: [russian word, tag info, russian word stem]
+		For punctuation, I'll just fill the triple with that punctuation mark (e.g., [",", ",", ","])
+		
+		So, you might iterate through the corpus as follows:
+		for sentence in self.corpus:
+			for word in sentence:
+				#do stuff
+		"""
+		self.corpus = []	#see above
+
+	"""
+	This method just interprets the first character in the tag string (which indicates part of speech)
+	This is the most basic information contained in the tag.
+	For more detailed information about a given word (gender, case, etc.), see the 
+	  following site for info on how to interpret the rest of the tag string:
+	    http://corpus.leeds.ac.uk/mocky/msd-ru.html
+	"""
+	def part_of_speech(self, word):
+		tag = word[1]
+		category = tag[0]
+		if category == 'N':
+			return 'noun'
+		elif category == 'V':
+			return 'verb'
+		elif category == 'A':
+			return 'adjective'
+		elif category == 'P':
+			return 'pronoun'
+		elif category == 'R':
+			return 'adverb'
+		elif category == 'S':
+			return 'adposition'
+		elif category == 'C':
+			return 'conjunction'
+		elif category == 'M':
+			return 'numeral'
+		elif category == 'Q':
+			return 'particle'
+		elif category == 'I':
+			return 'interjection'
+		elif category == 'Y':
+			return 'abbreviation'
+		elif category == 'X':
+			return 'residual'
+		return "punctuation"
+
+	def word_to_str(self, word):
+		return "%s\t%s\t%s" % (word[0], word[1], word[2]) 
+
+	def read_data(self, filename):
+		"""
+		Read in the corpus at the given filename
+		"""
+		
+		punctuation = ".,:;"
+		cur_sentence = []
+		f = open(filename, 'r')
+		for line in f:
+			line = unicode(line, encoding='utf-8')
+			split_line = line.split()
+			word = []
+			if len(split_line) > 0 and split_line[0] in punctuation:
+				punc = punctuation[punctuation.index(split_line[0])]
+				word = [punc, punc, punc]
+			elif len(split_line) == 3:
+				word = [split_line[0].lower(), split_line[1], split_line[2].lower()]
+			else:
+				print "ERROR: TagReader couldn't process line: ", line
+			
+			cur_sentence.append(word)
+			if len(word) > 0 and word[0] == '.':
+				self.corpus.append(cur_sentence)
+				cur_sentence = []
+
+def main(args):
+	tr = TagReader()
+	tr.read_data('../data/two_sentences_tagged.txt')
+	
+	for sentence in tr.corpus:
+		for word in sentence:
+			print tr.word_to_str(word) + "\t(%s)" %tr.part_of_speech(word)
+
+if __name__ == '__main__':
+	args = sys.argv[1:]
+	main(args)
--- python/translator.py
+++ python/translator.py
@@ -5,11 +5,12 @@ import sys
 import os
 import re
 
+from tag_reader import TagReader
+
 class Translator:
 
 	def __init__(self):
 		self.dictionary = {}
-		self.foreignText = []
 
 	def tokenize(self, line):
 		line = line.replace('.', '')
@@ -20,19 +21,6 @@ class Translator:
 		print line
 		return line
 
-	def read_data(self, filename):
-		"""
-		Given the location of the story, read it in one line at a time
-		"""
-		print "Reading in documents ..."
-		f = open(filename, 'r')
-		for line in f:
-			line = unicode(line, encoding='utf-8').lower()
-			line = self.tokenize(line)
-			print line
-			self.foreignText.append(line)
-			#print self.foreignText
-
 	def read_dict(self, filename):
 		"""Given the location of the dictionary, read it in and store it in the dictionary
 		"""
@@ -42,24 +30,43 @@ class Translator:
 			line[0] = unicode(line[0], encoding='utf-8')
 			line[1] = line[1][:-1]
 			self.dictionary[line[0]] = line[1]
-		#print self.dictionary
-
-	def translate(self):
-		sentence = []
-		for line in self.foreignText:
-			sentence = []
-			for word in line:
-				if word in self.dictionary:
-					sentence.append(self.dictionary[word])
-			print sentence
-			print '\n'
+	
+	def read_tagged_corpus(self, filename):
+		#Read in a corpus, as defined by the TagReader
+		tr = TagReader()
+		tr.read_data(filename)
+		return tr.corpus
+		
+	def translate(self, corpus_filename, tagged_corpus_filename):
+		print "BEGINNING TRANSLATION\n"
+		f = open(corpus_filename, 'r')
+		punctuation = ".,;:"
+		corpus = self.read_tagged_corpus(tagged_corpus_filename)
+		for sentence in corpus:
+			translation = []
+			for word_triple in sentence:
+				word = word_triple[0]
+				tag_info = word_triple[1]	#not used yet
+				lemma = word_triple[2]		#not used yet
+				if word in punctuation:
+					translation.append(word)
+				elif word in self.dictionary:
+					info = self.dictionary[word].split('.')
+					english_word = info[0]		#info[1], if it exists, would be the Case (dat, gen, etc.),
+												#  but the tagger should have provided this already
+					translation.append(english_word)
+			print "Original sentence:"
+			print f.readline()[:-1]
+			print "Translated as:"
+			print translation
+			print ""
+		print "DONE"
 
 
 def main(args):
 	translator = Translator()
-	translator.read_data('../data/Lady_With_Toy_Dog.txt')
 	translator.read_dict('../data/dictionary.txt')
-	translator.translate()
+	translator.translate('../data/two_sentences.txt', '../data/two_sentences_tagged.txt')
 
 if __name__ == '__main__':
 	args = sys.argv[1:]
